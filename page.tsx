a95035e430b6096927217c380907496a0e72e394
MFLow VNBnode <76662222+pnpn1212@users.noreply.github.com>
2023-10-29 12:15:25 +0700

    Update page.tsx

--- page.tsx
+++ page.tsx
@@ -1,6 +1,10 @@
 import { sql } from "@vercel/postgres";
 
-export default async function Cart({ params }) {
+export default async function Cart({
+  params
+} : {
+  params: { user: string }
+}): Promise<JSX.Element> {
   const { rows } = await sql`SELECT * from CARTS where user_id=${params.user}`;
 
   return (
